08a231e38462ce20a861771311e314e4b2d6e69c
Sweathadharan <sweathadharan1212@gmail.com>
2022-11-13 18:52:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Style.css";
+import Card from './Card';
+
+function App() {
+  return (
+   <>
+   <div className='container'>
+    <div className='row'>
+      <Card />
+    </div>
+
+   </div>
+   </>
+  );
+}
+
+export default App;
